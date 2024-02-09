e8ccf0974b48405c6441f502901c910f1ba47fde
Paul Kilar <pkilar@hotmail.com>
2024-02-08 20:23:06 -0500

    Update setup.sh
    Switched container to Debian 12

--- setup.sh
+++ setup.sh
@@ -1,31 +1,57 @@
 #!/usr/bin/env bash
 
+# project: https://github.com/fiveangle/proxmox_portainer_lxc
+# synopsis: Creates Portainer/docker environment nested within a Proxmox LXC container
+# author: Dave Johnson, fiveangle@gmail.com
+# date: 13mar2021
+# license: GPL 3.0 https://www.gnu.org/licenses/gpl-3.0.en.html
+# contribs: Dave Johnson <fiveangle@gmail.com> https://github.com/fiveangle
+#           Actpo Homoc https://github.com/Actpohomoc
+#           whiskerz007 https://github.com/whiskerz007
+
+# Set your desired container parameters
+PORTAINER_VERSION=portainer-ce  # 1.x=portainer, 2.x=portainer-ce
+HOSTNAME=portainer
+DISK_SIZE=16G
+
+
+
+# Create a temporary container environment setup script
+CONTAINER_ENV_SETUP_SCRIPT=$(mktemp)
+trap "rm -f $CONTAINER_ENV_SETUP_SCRIPT" 0 2 3 15
+
+###################################################
+#### BEGIN embedded CONTAINER_ENV_SETUP_SCRIPT
+###################################################
+cat > $CONTAINER_ENV_SETUP_SCRIPT <<EOF_MASTER
+#!/usr/bin/env bash
+
 # Setup script environment
 set -o errexit  #Exit immediately if a pipeline returns a non-zero status
 set -o errtrace #Trap ERR from shell functions, command substitutions, and commands from subshell
 set -o nounset  #Treat unset variables as an error
 set -o pipefail #Pipe will exit with last non-zero status if applicable
 shopt -s expand_aliases
-alias die='EXIT=$? LINE=$LINENO error_exit'
+alias die='EXIT=\$? LINE=\$LINENO error_exit'
 trap die ERR
 trap 'die "Script interrupted."' INT
 
 function error_exit() {
   trap - ERR
   local DEFAULT='Unknown failure occured.'
-  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
-  local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
-  msg "$FLAG $REASON"
-  exit $EXIT
+  local REASON="\e[97m\${1:-\$DEFAULT}\e[39m"
+  local FLAG="\e[91m[ERROR:LXC] \e[93m\$EXIT@\$LINE"
+  msg "\$FLAG \$REASON"
+  exit \$EXIT
 }
 function msg() {
-  local TEXT="$1"
-  echo -e "$TEXT"
+  local TEXT="\$1"
+  echo -e "\$TEXT"
 }
 
 # Prepare container OS
 msg "Setting up container OS..."
-sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+sed -i "/\$LANG/ s/\\(^# \\)//" /etc/locale.gen
 locale-gen >/dev/null
 apt-get -y purge openssh-{client,server} >/dev/null
 apt-get autoremove >/dev/null
@@ -37,14 +63,14 @@ apt-get -qqy upgrade &>/dev/null
 
 # Install prerequisites
 msg "Installing prerequisites..."
-apt-get -qqy install \
+apt-get -qqy install \\
     curl &>/dev/null
 
 # Customize Docker configuration
 msg "Customizing Docker..."
 DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
-mkdir -p $(dirname $DOCKER_CONFIG_PATH)
-cat >$DOCKER_CONFIG_PATH <<'EOF'
+mkdir -p \$(dirname \$DOCKER_CONFIG_PATH)
+cat >\$DOCKER_CONFIG_PATH <<'EOF'
 {
   "log-driver": "journald"
 }
@@ -55,26 +81,16 @@ msg "Installing Docker..."
 sh <(curl -sSL https://get.docker.com) &>/dev/null
 
 # Install Portainer
-msg "Installing Portainer 2x (portainer-ce)..."
+msg "Installing Portainer $PORTAINER_VERSION..."
 docker volume create portainer_data >/dev/null
-docker run -d \
-  -p 8000:8000 \
-  -p 9000:9000 \
-  --label com.centurylinklabs.watchtower.enable=true \
-  --name=portainer \
-  --restart=always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data \
-  portainer/portainer-ce &>/dev/null
-
-# Install Watchtower
-msg "Installing Watchtower..."
-docker run -d \
-  --name watchtower \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower \
-  --cleanup \
-  --label-enable &>/dev/null
+docker run -d \\
+  -p 8000:8000 \\
+  -p 9000:9000 \\
+  --name=portainer \\
+  --restart=unless-stopped \\
+  -v /var/run/docker.sock:/var/run/docker.sock \\
+  -v portainer_data:/data \\
+  portainer/$PORTAINER_VERSION &>/dev/null
 
 # Customize container
 msg "Customizing container..."
@@ -82,15 +98,198 @@ rm /etc/motd # Remove message of the day after login
 rm /etc/update-motd.d/10-uname # Remove kernel information after login
 touch ~/.hushlogin # Remove 'Last login: ' and mail notification after login
 GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
-mkdir -p $(dirname $GETTY_OVERRIDE)
-cat << EOF > $GETTY_OVERRIDE
+mkdir -p \$(dirname \$GETTY_OVERRIDE)
+cat << EOF > \$GETTY_OVERRIDE
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \\\$TERM
 EOF
 systemctl daemon-reload
-systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+systemctl restart \$(basename \$(dirname \$GETTY_OVERRIDE) | sed 's/\\.d//')
 
 # Cleanup container
 msg "Cleanup..."
+apt-get autoclean
 rm -rf /setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
+EOF_MASTER
+#################################################
+#### END embedded CONTAINER_ENV_SETUP_SCRIPT
+#################################################
+
+# Setup script environment
+set -o errexit  #Exit immediately if a pipeline returns a non-zero status
+set -o errtrace #Trap ERR from shell functions, command substitutions, and commands from subshell
+set -o nounset  #Treat unset variables as an error
+set -o pipefail #Pipe will exit with last non-zero status if applicable
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
+trap cleanup EXIT
+
+function error_exit() {
+  trap - ERR
+  local DEFAULT='Unknown failure occured.'
+  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
+  local FLAG="\e[91m[ERROR] \e[93m$EXIT@$LINE"
+  msg "$FLAG $REASON"
+  [ ! -z ${CTID-} ] && cleanup_ctid
+  exit $EXIT
+}
+function warn() {
+  local REASON="\e[97m$1\e[39m"
+  local FLAG="\e[93m[WARNING]\e[39m"
+  msg "$FLAG $REASON"
+}
+function info() {
+  local REASON="$1"
+  local FLAG="\e[36m[INFO]\e[39m"
+  msg "$FLAG $REASON"
+}
+function msg() {
+  local TEXT="$1"
+  echo -e "$TEXT"
+}
+function cleanup_ctid() {
+  if [ ! -z ${MOUNT+x} ]; then
+    pct unmount $CTID
+  fi
+  if $(pct status $CTID &>/dev/null); then
+    if [ "$(pct status $CTID | awk '{print $2}')" == "running" ]; then
+      pct stop $CTID
+    fi
+    pct destroy $CTID
+  elif [ "$(pvesm list $STORAGE --vmid $CTID)" != "" ]; then
+    pvesm free $ROOTFS
+  fi
+}
+function cleanup() {
+  popd >/dev/null
+  rm -rf $TEMP_DIR
+}
+function load_module() {
+  if ! $(lsmod | grep -Fq $1); then
+    modprobe $1 &>/dev/null || \
+      die "Failed to load '$1' module."
+  fi
+  MODULES_PATH=/etc/modules
+  if ! $(grep -Fxq "$1" $MODULES_PATH); then
+    echo "$1" >> $MODULES_PATH || \
+      die "Failed to add '$1' module to load at boot."
+  fi
+}
+TEMP_DIR=$(mktemp -d)
+pushd $TEMP_DIR >/dev/null
+
+# Download setup script
+#wget -qL https://github.com/fiveangle/proxmox_portainer_lxc/raw/master/setup.sh
+#cp /root/install/setup.sh ./
+
+# Detect modules and automatically load at boot
+#load_module aufs
+load_module overlay
+
+# Select the storage location
+while read -r line; do
+  TAG=$(echo $line | awk '{print $1}')
+  TYPE=$(echo $line | awk '{printf "%-10s", $2}')
+  FREE=$(echo $line | numfmt --field 4-6 --from-unit=K --to=iec --format %.2f | awk '{printf( "%9sB", $6)}')
+  ITEM="  Type: $TYPE Free: $FREE "
+  OFFSET=2
+  if [[ $((${#ITEM} + $OFFSET)) -gt ${MSG_MAX_LENGTH:-} ]]; then
+    MSG_MAX_LENGTH=$((${#ITEM} + $OFFSET))
+  fi
+  STORAGE_MENU+=( "$TAG" "$ITEM" "OFF" )
+done < <(pvesm status -content rootdir | awk 'NR>1')
+if [ $((${#STORAGE_MENU[@]}/3)) -eq 0 ]; then
+  warn "'Container' needs to be selected for at least one storage location."
+  die "Unable to detect valid storage location."
+elif [ $((${#STORAGE_MENU[@]}/3)) -eq 1 ]; then
+  STORAGE=${STORAGE_MENU[0]}
+else
+  while [ -z "${STORAGE:+x}" ]; do
+    STORAGE=$(whiptail --title "Storage Pools" --radiolist \
+    "Which storage pool you would like to use for the container?\n\n" \
+    16 $(($MSG_MAX_LENGTH + 23)) 6 \
+    "${STORAGE_MENU[@]}" 3>&1 1>&2 2>&3) || exit
+  done
+fi
+info "Using '$STORAGE' for storage location."
+
+# Get the next guest VM/LXC ID
+CTID=$(pvesh get /cluster/nextid)
+info "Container ID is $CTID."
+
+# Download the latest Debian 12 LXC template
+msg "Updating LXC template list..."
+pveam update >/dev/null
+msg "Downloading LXC template..."
+OSTYPE=debian
+OSVERSION=${OSTYPE}-12
+mapfile -t TEMPLATES < <(pveam available -section system | sed -n "s/.*\($OSVERSION.*\)/\1/p" | sort -t - -k 2 -V)
+TEMPLATE="${TEMPLATES[-1]}"
+pveam download template $TEMPLATE >/dev/null ||
+  die "A problem occurred while downloading the LXC template."
+
+# Create variables for container disk
+STORAGE_TYPE=$(pvesm status -storage $STORAGE | awk 'NR>1 {print $2}')
+case $STORAGE_TYPE in
+  dir|nfs)
+    DISK_EXT=".raw"
+    DISK_REF="$CTID/"
+    ;;
+  zfspool)
+    DISK_PREFIX="subvol"
+    DISK_FORMAT="subvol"
+    ;;
+esac
+DISK=${DISK_PREFIX:-vm}-${CTID}-disk-0${DISK_EXT-}
+ROOTFS=${STORAGE}:${DISK_REF-}${DISK}
+
+# Create LXC
+msg "Creating LXC container..."
+pvesm alloc $STORAGE $CTID $DISK $DISK_SIZE --format ${DISK_FORMAT:-raw} >/dev/null
+if [ "$STORAGE_TYPE" == "zfspool" ]; then
+  warn "Some containers may not work properly due to ZFS not supporting 'fallocate'."
+else
+  mkfs.ext4 $(pvesm path $ROOTFS) &>/dev/null
+fi
+ARCH=$(dpkg --print-architecture)
+TEMPLATE_STRING="template:vztmpl/${TEMPLATE}"
+pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
+  -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 \
+  -ostype $OSTYPE -rootfs $ROOTFS,size=$DISK_SIZE -storage $STORAGE >/dev/null
+
+# Modify LXC permissions to support Docker
+LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
+cat <<EOF >> $LXC_CONFIG
+lxc.cgroup.devices.allow: a
+lxc.cap.drop:
+EOF
+
+# Set container description
+pct set $CTID -description "Access Portainer interface using the following URL:
+
+http://<IP_ADDRESS>:9000"
+
+# Set container timezone to match host
+MOUNT=$(pct mount $CTID | cut -d"'" -f 2)
+ln -fs $(readlink /etc/localtime) ${MOUNT}/etc/localtime
+pct unmount $CTID && unset MOUNT
+
+# Setup container
+msg "Starting LXC container..."
+pct start $CTID
+pct push $CTID $CONTAINER_ENV_SETUP_SCRIPT /setup.sh -perms 755
+pct exec $CTID /setup.sh
+
+# Get network details and show the completion message
+IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
+info "Successfully created Portainer LXC to container ID $CTID."
+msg "
+
+Portainer is reachable by going to the following URLs:
+
+      http://${IP}:9000
+      http://${HOSTNAME}.local:9000
+
+"
